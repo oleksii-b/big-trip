7acdd8c79b1d094a27df5f27029e0bbe9653f8cf
alex <alexx.bel@ukr.net>
2019-11-26 16:45:21 +0200

    no message

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './pages/home/home-page.component';
 import { SortingComponent } from './components/sorting/sorting.component';
+import { TripDaysComponent } from './components/trip-days/trip-days.component';
 
 @NgModule({
   declarations: [
     HomePageComponent,
     SortingComponent,
+    TripDaysComponent,
   ],
   imports: [
     CommonModule,
