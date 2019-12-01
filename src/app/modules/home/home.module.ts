2a964446ad730652d792ae4c7a30d6b14e21e061
alex <alexx.bel@ukr.net>
2019-12-01 20:55:17 +0200

    no message

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,11 +3,9 @@ import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
 
-
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './pages/home/home-page.component';
 import { SortingComponent } from './components/sorting/sorting.component';
-import { NewEventComponent } from './components/new-event/new-event.component';
 import { TripDaysComponent } from './components/trip-days/trip-days.component';
 import { TripEventsComponent } from './components/trip-events/trip-events.component';
 
@@ -15,7 +13,6 @@ import { TripEventsComponent } from './components/trip-events/trip-events.compon
   declarations: [
     HomePageComponent,
     SortingComponent,
-    NewEventComponent,
     TripDaysComponent,
     TripEventsComponent,
   ],
