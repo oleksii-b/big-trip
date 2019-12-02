59098fc8e45a55b49d52e0d1cdd16a3f0beeadd0
OM.KLYMENKO <klymenko8olexandr@gmail.com>
2019-12-02 22:15:00 +0200

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TripEventsDataService } from './modules/home/services/trip-events-data.service';
 
 
 @NgModule({
@@ -15,8 +13,7 @@ import { TripEventsDataService } from './modules/home/services/trip-events-data.
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    InMemoryWebApiModule.forRoot(TripEventsDataService)
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './pages/home/home-page.component';
@@ -9,6 +10,7 @@ import { SortingComponent } from './components/sorting/sorting.component';
 import { NewEventComponent } from './components/new-event/new-event.component';
 import { TripDaysComponent } from './components/trip-days/trip-days.component';
 import { TripEventsComponent } from './components/trip-events/trip-events.component';
+import { TripEventsDataService } from './services/trip-events-data.service';
 
 @NgModule({
   declarations: [
@@ -23,6 +25,7 @@ import { TripEventsComponent } from './components/trip-events/trip-events.compon
     HomeRoutingModule,
     ButtonModule,
     DialogModule,
+    InMemoryWebApiModule.forRoot(TripEventsDataService)
   ]
 })
 export class HomeModule { }
