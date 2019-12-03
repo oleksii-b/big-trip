60d988a9228376f939c53bb933a2c4eb637d90e5
OM.KLYMENKO <klymenko8olexandr@gmail.com>
2019-12-03 22:04:03 +0200

    no message

--- src/app/modules/home/services/trip-events.service.ts
+++ src/app/modules/home/services/trip-events.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { TripEventsDataService } from './trip-events-data.service';
 
 @Injectable()
 export class TripEventsService {
-  constructor(private service: TripEventsDataService) {
+  private apiUrl = 'api/points';
+  // constructor(private http: TripEventsDataService) {
 
-  }
+  // }
 }
